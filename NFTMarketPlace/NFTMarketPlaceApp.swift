8d8ff5ee37f3aa2e1a092eff535c5b6f0322ed5b
Morsy Mohamed Elsokary <morsysoker@gmail.com>
2022-11-21 16:49:40 +0200

    Initial Commit

---
+++ NFTMarketPlace/NFTMarketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFTMarketPlaceApp.swift
+//  NFTMarketPlace
+//
+//  Created by MorsyElsokary on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct NFTMarketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
